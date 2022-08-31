a56609fb2de96a5efb6c2981a28b8785230f70d8
satuhin95 <saifcm95@gmail.com>
2022-08-31 16:49:54 +0600

    update

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL:"https://react-video-gallery.herokuapp.com/api/",
+    baseURL:"https://react-video-gallery.herokuapp.com/api",
 });
 
 export default axiosInstance;
