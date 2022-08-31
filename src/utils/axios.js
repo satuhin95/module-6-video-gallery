2486d65377fce42eb9fff444b769ad1535c5d783
satuhin95 <saifcm95@gmail.com>
2022-08-31 16:35:49 +0600

    deploy brance

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:9000",
+    baseURL:"https://react-video-gallery.herokuapp.com/api/",
 });
 
 export default axiosInstance;
